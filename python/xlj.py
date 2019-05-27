ed659f85798673245e8577607894cfb541e0563d
dtwguidgit <38973517+dtwguidgit@users.noreply.github.com>
2019-05-27 15:44:02 +0800

    add python5

--- python/xlj.py
+++ python/xlj.py
@@ -5,17 +5,21 @@ url = 'https://hao8i.github.io/dh123/'
 html = requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 articles = []
-for article in soup.find_all(class_='myweb_site_align'):
-    print(article)
-    u = article.select('a')
-    print(str(u))
-    tip = re.findall(r'(?<=title=").+?(?=")', str(u))[0]
-    title = u[0].get_text()
-    link = 'https://hao8i.github.io/' + re.findall(r'(?<=href=").+?(?=")', str(u))[0]
-    articles.append([title, link, tip])
+for myweb in soup.find_all(class_='myweb'):
+    myweb_u = myweb.select('a')
+    grp_title = myweb_u[0].get_text()
+    myweb_site_align = myweb.select('.myweb_site_align')
+    for article in myweb_site_align:
+        print(article)
+        u = article.select('a')
+        print(str(u))
+        tip = re.findall(r'(?<=title=").+?(?=")', str(u))[0]
+        title = u[0].get_text()
+        link = 'https://hao8i.github.io/' + re.findall(r'(?<=href=").+?(?=")', str(u))[0]
+        articles.append([grp_title, title, link, tip])
 
 with open(r'document\xlj.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(['网站标题', '网站地址', '网站提示'])
+    writer.writerow(['网站组标题', '网站标题', '网站地址', '网站提示'])
     for row in articles:
         writer.writerow(row)
